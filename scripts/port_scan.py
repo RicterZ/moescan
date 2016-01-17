0bcec8b2dcadc258357f624963d2cd2d39d8f8cc
Ricter Z <ricterzheng@gmail.com>
2016-01-17 21:15:21 +0800

    remove .git/ folder of 2 tools (prevent their api changed)

--- scripts/port_scan.py
+++ scripts/port_scan.py
@@ -13,11 +13,12 @@ class Exploit(exploit.BaseExploit):
     def help(self):
         print('Port scan use python-nmap')
         print('extra_arg: ')
+        print('-' * 30)
+        print('arg: arguments of nmap')
 
     def exploit(self):
         n = nmap.PortScanner()
         print('Scan {} ports: {}'.format(self.target, self.port))
         arg = self.extra_arg.get('arg') if not self.extra_arg.get('arg') is None else '-sV -Pn'
         result = n.scan(hosts=self.target, ports=self.port, arguments=arg)
-        print(n.command_line())
         print(result)
